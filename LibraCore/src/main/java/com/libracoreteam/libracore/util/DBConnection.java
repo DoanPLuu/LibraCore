7e12fd958ee94fbf75c8bec0a4c81de501edbf69
LuuD04N <doanphongluu082@gmail.com>
2026-01-02 22:30:48 +0700

    feat: update dependencies, pomml and dbconnection

--- LibraCore/src/main/java/com/libracoreteam/libracore/util/DBConnection.java
+++ LibraCore/src/main/java/com/libracoreteam/libracore/util/DBConnection.java
@@ -4,10 +4,32 @@
  */
 package com.libracoreteam.libracore.util;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
 /**
  *
  * @author luuis
  */
 public class DBConnection {
-    
+    private static final HikariDataSource ds;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(ConfigUtility.getConfig("db.url"));
+        config.setUsername(ConfigUtility.getConfig("db.user"));
+        config.setPassword(ConfigUtility.getConfig("db.password"));
+        config.setMaximumPoolSize(10);
+
+        ds = new HikariDataSource(config);
+    }
+
+    private DBConnection() {}
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
 }
